8ad5ef1f6894bb08d120d5941572fe8c9b283319
VolodymyrButs <wowabuz@gmail.com>
2020-07-24 21:21:56 +0300

    Updates

---
+++ precache-manifest.d88e6009f26dd6da8b69afdeb96ea386.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9b8b929f24e488839d66e61045803f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "593d0dd7755141fc81c7",
+    "url": "/static/js/2.c6e2249c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c6e2249c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bedea186dcb336e3d98f",
+    "url": "/static/js/main.47d956a6.chunk.js"
+  },
+  {
+    "revision": "2335538a693e80451134",
+    "url": "/static/js/runtime-main.2047469b.js"
+  }
+]);
