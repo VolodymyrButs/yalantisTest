99b514ffcb21edee7a71c46cbfdf1db9f22910ac
VolodymyrButs <wowabuz@gmail.com>
2020-07-25 01:57:33 +0300

    Updates

---
+++ precache-manifest.9a1be406e59a83764b41ba766ce45b74.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac5ce45100ba04349f10e24eb151131e",
+    "url": "/yalantisTest/index.html"
+  },
+  {
+    "revision": "4909d71eb8621c09f983",
+    "url": "/yalantisTest/static/js/2.c7b678a7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/yalantisTest/static/js/2.c7b678a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17fd276fcf6143539035",
+    "url": "/yalantisTest/static/js/main.7255451a.chunk.js"
+  },
+  {
+    "revision": "c12658b0f7cd4a58b8fd",
+    "url": "/yalantisTest/static/js/runtime-main.bacbd59b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d88e6009f26dd6da8b69afdeb96ea386.js"
+  "/yalantisTest/precache-manifest.9a1be406e59a83764b41ba766ce45b74.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/yalantisTest/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
